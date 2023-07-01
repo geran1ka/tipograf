58058a04d60adc61cd546cfda62684a0f9a36387
geran1ka <roman.khoruzhy@gmail.com>
2023-07-01 18:31:36 +0300

    переделана функция getTipedText

--- script/getTipedText.js
+++ script/getTipedText.js
@@ -1,15 +1,34 @@
+/*
 export const getTipedText = (str) => {
   const pravila = {
     'step1' : [/(^|\s)(в|без|до|для|за|через|над|по|из|у|около|под|о|про|на|к|перед|при|с|между)\s/gi, '$1$2&nbsp;'],
     'step2' : [/©/gi, '&copy;'],
     'step3' : [/#|№/gi, '&#8470;'],
     'step4' : [/\s-\s/gi, '&nbsp&mdash; '],
-    'step5' : [/«|/gi, '&laquo;'],
-    'step5' : [/»/gi, '&raquo;'],
+    'step5' : [/«|(?<=\s)"/gi, '&laquo;'],
+    'step5' : [/»|(?<=\S)"/gi, '&raquo;'],
   }
   let newStr = str
   for (let key in pravila) {
     newStr = newStr.replace(pravila[key][0], pravila[key][1])
   }
+  return newStr
+};
+*/
+
+export const getTipedText = (str) => {
+  const regexpStepOne = /(^|\s)(в|без|до|для|за|через|над|по|из|у|около|под|о|про|на|к|перед|при|с|между)\s/gi
+  const regexpFiveStepTwo = /©/gi;
+  const regexpFiveStepThre = /#|№/gi;
+  const regexpFiveStepFour = /\s-\s/gi;
+  const regexpFiveStepFive = /«|(?<=\s)"/gi;
+  const regexpFiveStepSix = /»|(?<=\S)"/gi;
+  
+  let newStr = str.replace(regexpStepOne, '$1$2&nbsp;')
+  newStr = newStr.replace(regexpFiveStepTwo, '&copy;')
+  newStr = newStr.replace(regexpFiveStepThre, '&#8470;')
+  newStr = newStr.replace(regexpFiveStepFour, '&nbsp&mdash; ')
+  newStr = newStr.replace(regexpFiveStepFive, '&laquo;')
+  newStr = newStr.replace(regexpFiveStepSix, '&raquo;')
   return newStr
 };
